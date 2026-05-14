ef39bbbc84915f18390522d43da6d2660b927a9d
mvcobos <vcobos178@gmail.com>
2026-05-14 10:53:41 -0500

    Student-detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { StudentsPage } from './features/students/pages/students-page/students-page';
+import { StudentDetailPage } from './features/students/pages/student-detail-page/student-detail-page';
+import { HomePages } from './features/home/pages/home-pages/home-pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePages},
+    { path: 'students', component: StudentsPage},
+    { path: 'students/:id', component: StudentDetailPage},
+    //redireccionamiento
+    { path: '**', redirectTo: ''}
+];
---
+++ src/app/features/students/pages/students-page/students-page.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-students-page',
+  imports: [RouterLink],
+  templateUrl: './students-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StudentsPage {
+  readonly students = signal([
+    {id:1, name: "Vero Cobos"},
+    {id:2, name: "Diego Perez"},
+    {id:3, name: "Maria Pinos"},
+    {id:4, name: "Alvaro Lomas"},
+    {id:5, name: "Alexandra Alcivar"}
+  ]);
+}
